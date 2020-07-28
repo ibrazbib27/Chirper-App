f07796668c2b8dc396dec4c35ea2f4e86055df9c
ibrazbib27 <ibrahim.zbib@macys.com>
2020-07-28 16:20:46 -0400

    Update ImageSection.jsx

--- src/components/CreateCard/CardParts/Image/ImageSection.jsx
+++ src/components/CreateCard/CardParts/Image/ImageSection.jsx
@@ -1,21 +1,15 @@
-import React from 'react';
-import Row from 'react-bootstrap/Row';
-import ChirperImg from './Picture/ChirperImg';
-import UploadImgField from './Input/UploadImgField';
-
-
-
+import React from "react";
+import Row from "react-bootstrap/Row";
+import ChirperImg from "./Picture/ChirperImg";
+import UploadImgField from "./Input/UploadImgField";
 
 const ImageSection = (props) => {
-
- 
-return(
-
+  return (
     <Row className="mb-5">
- <ChirperImg  cards={props.cards}update={props.input} link={props.link}/>
- {props.input ? <UploadImgField  modal={props.modal} /> : null}
-          </Row>    
-);
-}
+      <ChirperImg cards={props.cards} update={props.input} link={props.link} />
+      {props.input ? <UploadImgField modal={props.modal} /> : null}
+    </Row>
+  );
+};
 
 export default ImageSection;
