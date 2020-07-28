10135caa1b1175022942eef7ca83b50e5e633aad
ibrazbib27 <ibrahim.zbib@macys.com>
2020-07-28 16:18:30 -0400

    Update MsgInput.jsx

--- src/components/CreateCard/CardParts/Message/MsgInput.jsx
+++ src/components/CreateCard/CardParts/Message/MsgInput.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import Tooltip from 'react-bootstrap/Tooltip';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+import React from "react";
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function renderDescTooltip(props) {
   return (
     <Tooltip id="button-tooltip" {...props}>
-     Enter a description of your Chirper here!
+      Enter a description of your Chirper here!
     </Tooltip>
   );
 }
 
-
 const MsgInput = (props) => {
-
- 
-return(
-   
-      
-       <Row className={props.change ? "mb-3" : "my-5"}>
-            <Col xs={12}  >
-           <OverlayTrigger
-    placement="bottom"
-    delay={{ show: 250, hide: 400 }}
-    overlay={renderDescTooltip}
->
-            <textarea className="form-control" id="textarea" defaultValue={props.change ? props.myInput : ""} rows="3" placeholder="Chirper Description"></textarea>
-           </OverlayTrigger>
-             </Col>
-      </Row>
- 
-);
-}
+  return (
+    <Row className={props.change ? "mb-3" : "my-5"}>
+      <Col xs={12}>
+        <OverlayTrigger
+          placement="bottom"
+          delay={{ show: 250, hide: 400 }}
+          overlay={renderDescTooltip}
+        >
+          <textarea
+            className="form-control"
+            id="textarea"
+            defaultValue={props.change ? props.myInput : ""}
+            rows="3"
+            placeholder="Chirper Description"
+          ></textarea>
+        </OverlayTrigger>
+      </Col>
+    </Row>
+  );
+};
 
 export default MsgInput;
