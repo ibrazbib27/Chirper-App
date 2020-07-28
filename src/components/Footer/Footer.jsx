78cbb4aeee4c72870e591eb688c40609e8b1852b
ibrazbib27 <ibrahim.zbib@macys.com>
2020-07-28 16:15:57 -0400

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,34 +1,23 @@
-import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Footer = (props) => {
+  return (
+    <Row className="w-100   justify-content-around bg-dark mt-auto">
+      <Col xs={12} className="mx-auto mt-2">
+        <Row className="w-25  mx-auto justify-content-between">
+          <i className="fab fa-facebook-square fa-2x text-primary"></i>
+          <i className="fab fa-twitter-square fa-2x text-white"></i>
+          <i className="fab fa-instagram-square fa-2x text-danger"></i>
+        </Row>
+      </Col>
 
- 
-return(
-<Row className="w-100   justify-content-around bg-dark mt-auto">
-  
-      
-
-
-<Col xs={12} className="mx-auto mt-2">
-<Row className="w-25  mx-auto justify-content-between">
-<i className="fab fa-facebook-square fa-2x text-primary"></i>
-<i  className="fab fa-twitter-square fa-2x text-white"></i>
-<i className="fab fa-instagram-square fa-2x text-danger"></i>
-</Row>
-</Col>
-
-<Col xs={12} className="text-center smaller font-italic text-white mt-1">
- © 2020 Copyright: ChirperApp.com
-    </Col>
-
-
-   
-   
-</Row>
- 
-    );
-}
+      <Col xs={12} className="text-center smaller font-italic text-white mt-1">
+        © 2020 Copyright: ChirperApp.com
+      </Col>
+    </Row>
+  );
+};
 
 export default Footer;
