c2711a8570ff41cd81efc87e8f6243e03cd9eb31
ibrazbib27 <ibrahim.zbib@macys.com>
2020-07-28 16:20:13 -0400

    Update ChirperImg.jsx

--- src/components/CreateCard/CardParts/Image/Picture/ChirperImg.jsx
+++ src/components/CreateCard/CardParts/Image/Picture/ChirperImg.jsx
@@ -1,47 +1,36 @@
-import React, {Component} from 'react';
-import Col from 'react-bootstrap/Col';
-import Figure from 'react-bootstrap/Figure';
+import React, { Component } from "react";
+import Col from "react-bootstrap/Col";
+import Figure from "react-bootstrap/Figure";
 
-class ChirperImg extends Component{
-    constructor(){
-        super();
-        this.state = {
-           img : false,
-           srcImg : ""
-           
-        }
-   
-    }
-newSrc = (mySrc) => {
-   
-      this.setState({ img: true ,
-                      srcImg : mySrc
-                    });
- }
-    
-render(){
+class ChirperImg extends Component {
+  constructor() {
+    super();
+    this.state = {
+      img: false,
+      srcImg: "",
+    };
+  }
+  newSrc = (mySrc) => {
+    this.setState({ img: true, srcImg: mySrc });
+  };
 
-
- 
-return(
-
-         <Col xs={12} > 
-    <Figure className="border border-dark">
-  <Figure.Image
-    width={300}
-    height={300}
-    id="IMG"
-    src={this.state.img ? this.state.srcImg : this.props.link}
-    onLoad={(e) => {this.newSrc(e.target.src);} }
-
-  />
-</Figure>
-        </Col>
-   
-  
-  
-      
-);
-} }
+  render() {
+    return (
+      <Col xs={12}>
+        <Figure className="border border-dark">
+          <Figure.Image
+            width={300}
+            height={300}
+            id="IMG"
+            src={this.state.img ? this.state.srcImg : this.props.link}
+            onLoad={(e) => {
+              this.newSrc(e.target.src);
+            }}
+          />
+        </Figure>
+      </Col>
+    );
+  }
+}
 
 export default ChirperImg;
