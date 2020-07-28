c3a40885ccbb63f41b6f3eb340e2af4c5c88f3a5
ibrazbib27 <ibrahim.zbib@macys.com>
2020-07-28 16:19:06 -0400

    Update MsgOutput.jsx

--- src/components/CreateCard/CardParts/Message/MsgOutput.jsx
+++ src/components/CreateCard/CardParts/Message/MsgOutput.jsx
@@ -1,27 +1,17 @@
-import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-
-
-
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const MsgOutput = (props) => {
-
- 
-return(
-
-       <Row className="my-5">
-            <Col xs={12}  >
-          
-               <section >
-                   <p className="font-italic">{props.text}</p>
-               </section>
-          
-             </Col>
-      </Row>
- 
-);
-}
+  return (
+    <Row className="my-5">
+      <Col xs={12}>
+        <section>
+          <p className="font-italic">{props.text}</p>
+        </section>
+      </Col>
+    </Row>
+  );
+};
 
 export default MsgOutput;
